b598495ddafea666694e32bbfc87bd884f06fe43
Rokas Brazdžionis <rokas.brazdzionis@visma.com>
2022-03-20 12:12:49 +0200

    refactor the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,53 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { TodoService } from './todo.service';
+import { TodoItem } from './app.models';
+
+export const TASK_COLLECTION_KEY = 'taskCollection';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  @ViewChild('taskInputRef') taskInputRef!: ElementRef<HTMLInputElement>;
+  todoList: TodoItem[] = [];
+  newTask = '';
 
-  constructor(public todoService: TodoService) {}
+  @ViewChild('newTaskInputRef') newTaskInputRef!: ElementRef<HTMLInputElement>;
 
-  onSubmit(form: NgForm): void {
+  ngOnInit(): void {
+    this.load();
+  }
+
+  add(form: NgForm): void {
     if (form.invalid) {
       return;
     }
 
-    const { newTask } = form.value;
-    this.todoService.add(newTask);
+    const newTask: TodoItem = { title: this.newTask, done: false };
+    this.todoList = [...this.todoList, newTask];
+    this.newTask = '';
+    this.newTaskInputRef.nativeElement.focus();
+    this.save();
+  }
 
-    form.resetForm({ newTask: '' });
-    this.taskInputRef.nativeElement.focus();
+  remove(index: number): void {
+    this.todoList = this.todoList.filter((_, i) => i !== index);
+    this.save();
+  }
+
+  toggle(index: number, status: boolean): void {
+    const todoItem = this.todoList.find((_, i) => i === index);
+    todoItem!.done = status;
+    this.save();
+  }
+
+  private load(): void {
+    this.todoList = JSON.parse(
+      localStorage.getItem(TASK_COLLECTION_KEY) ?? '[]'
+    );
+  }
+
+  private save(): void {
+    localStorage.setItem(TASK_COLLECTION_KEY, JSON.stringify(this.todoList));
   }
 }
